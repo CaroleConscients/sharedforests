3a7564fa43c03be8e41dbce00743a1c844928475
Carole Piette <carole@conscients.com>
2016-11-28 19:04:02 +0100

    save before adding occasion to certificate

--- app/controllers/certificates_controller.rb
+++ app/controllers/certificates_controller.rb
@@ -5,17 +5,36 @@ class CertificatesController < ApplicationController
   end
 
   def create
-
+    @parcel = Parcel.find(params[:parcel_id])
+    @certificate = Certificate.new(certificate_params)
+    @certificate.parcel = @certificate
+    @certificate.save
+    redirect_to certificate_path(@certificate)
   end
 
   def edit
     @certificate = Certificate.find(params[:id])
-
   end
 
   def update
 
   end
 
+  private
+
+  def certificate_params
+    params.require(:certificate).permit(:parcel_id, :name, :occasion, :message, :date, :trees_quantity)
+  end
 end
 
+
+
+# t.integer  "unique_number"
+#     t.integer  "parcel_id"
+#     t.string   "name"
+#     t.text     "message"
+#     t.datetime "date"
+#     t.integer  "trees_quantity"
+#     t.datetime "created_at",     null: false
+#     t.datetime "updated_at",     null: false
+#     t.index ["parcel_id"], name: "index_certificates_on_parcel_id", using: :btree
